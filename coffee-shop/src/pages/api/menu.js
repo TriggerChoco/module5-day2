a25de48ec8de1f16dc5d3deea7963936f7b85237
Nate <foxyfan23@gmail.com>
2024-06-22 11:36:48 -0500

    some stuff done

--- coffee-shop/src/pages/api/menu.js
+++ coffee-shop/src/pages/api/menu.js
@@ -1,7 +1,10 @@
+import {db} from "@/server/db";
+
 export default function handler(req, res) {
     if (req.method === 'GET') {
-        res.status(200).json({ name: "John Doe" });
+        const menuItems = db.menu.get()
+        res.status(200).json({ menu: menuItems });
     } else {
-        res.status(404).json({ message: "We only support GET requests." })
+        res.status(404).json({ message: "We only support GET requests." });
     }
   }
--- coffee-shop/src/pages/index.js
+++ coffee-shop/src/pages/index.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
+
+  const [menu, setMenu] = useState([]);
+
+  useEffect(async () => {
+    async function loadData() {
+      const res = await fetch("/api/menu");
+      const data = await res.json();
+      setMenu(data.menu);
+    }
+    loadData();
+  }, [])
+  
+  if (menu.length === 0) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <h1>Welcome to Coffee Shop</h1>
+      <h4>Here is our menu</h4>
+      {menu.map[(item) => {
+        return (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </div>
+        )
+      }]}
     </>
   );
 }
