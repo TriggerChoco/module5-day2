b343e59bc4068650f5ee3d1fba7e75bde83bb0a2
Nate <foxyfan23@gmail.com>
2024-07-01 19:43:29 -0500

    coffee shop page with functionality

--- coffee-shop/src/pages/cart.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    delete(menuItemId) {
-        const db = readDb();
-        db.cart = db.cart.filter((item) => item.id !== menuItemId);
-        writeDb(db);
-      },
-}
